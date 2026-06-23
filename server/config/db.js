9c38e6a830c071995ed3096f04accc4a6d2f6108
Parth Panchal <panchalparth93@yahoo.in>
2026-06-23 14:20:00 +0530

    Improve MongoDB error logs

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("MONGO_URL:", process.env.MONGO_URL);
-
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB Error:");
+    console.error(error);
+
     process.exit(1);
   }
 };
