6d46484ad73747846c1d0ec9dc073479f5d03c0d
Parth Panchal <panchalparth93@yahoo.in>
2026-06-26 16:05:15 +0530

    remove extra api

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app.vercel.app"
+      "https://trrip-ai-itinerary-generator.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
